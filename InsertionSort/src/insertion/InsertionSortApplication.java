143b2faf8dba0a1a4344eff1a3c3ff6f4c52b867
HasanZian <hasanzian@gmail.com>
2022-05-15 21:25:14 +0530

    added insertion sort algo

---
+++ InsertionSort/src/insertion/InsertionSortApplication.java
@@ -0,0 +1,26 @@
+package insertion;
+
+public class InsertionSortApplication {
+
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 1, -22, -1, 100, 3, 21, 10, 60, 12, 189, -6, -11, 90, 0};
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+
+            int newElement = intArray[firstUnsortedIndex];
+
+            for (int i = 0; i < intArray.length ; i++) {
+
+
+            }
+
+
+        }
+
+
+
+        for (int j : intArray) {
+            System.out.println(j);
+        }
+    }
+}
